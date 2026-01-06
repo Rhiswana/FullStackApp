3c4c435130ccd1c681c62e9b4de28ce63ff9e67b
Rhiswana <rhiswanabegam@gmail.com>
2026-01-06 22:42:14 +0530

    first prac .py

---
+++ Documents/Java-DSA/PythonDsa/dailydsa.py
@@ -0,0 +1,17 @@
+#largest element in array
+class Solution:
+    def largest(self, arr):
+        max=arr[0]
+        for i in arr:
+            if max<i:
+                max=i
+        return max
+            
+    
+        
+                
+             
+      
+        
+        
+        
